a2f43e1e8d8b1c17e7d3ec35b44d5d3d6d67d781
pomindeoq <mindaugas.dirma@gmail.com>
2017-11-15 09:20:57 +0100

    fixed inputs

--- scr/wwwroot/js/site.js
+++ scr/wwwroot/js/site.js
@@ -1,32 +1,21 @@
 ﻿// Write your JavaScript code.
 
 $(document).ready(function () {
-    var next = 1;
+    
     $(".add-more").click(function (e) {
-        e.preventDefault();
-        next = next + 1;
-        var addto = "#field";
-        var addRemove = "#field" + (next);      
-        var newIn = '<div class="input-group" id="newField' + next + '"><input autocomplete="off" class="form-control" id="field' + next + '" name="field' + next + '" type="text" >';
+        e.preventDefault();       
+        var addto = "#items";             
+        var newIn = '<div class="input-group"><input autocomplete="off" class="form-control" type="text" ><span class="input-group-btn"><button class="btn btn-danger remove-me" >-</button></span></div>';
         var newInput = $(newIn);
-        var removeBtn = '<span class="input-group-btn"><button id="remove' + (next) + '" class="btn btn-danger remove-me" >-</button></span></div>';
-        var removeButton = $(removeBtn);
-        $(addto).after(newInput);
-        $(addRemove).after(removeButton);
-        $("#field").attr('data-source', $(addto).attr('data-source'));
-        $("#count").val(next);
-
+        $(addto).(newInput);      
+        
+       //$(addto).attr('data-source', $(addto).attr('data-source'));     
+        
         $('.remove-me').click(function (e) {
-            e.preventDefault();
-            var fieldNum = this.id.charAt(this.id.length -1);
-            var fieldID = "#field" + fieldNum;
-            var newFieldID = "#newField" + fieldNum;
-            $(this).remove();
-            $(fieldID).remove();
-            $(newFieldID).remove();
+            e.preventDefault();            
+            $(this).parent().parent().remove();          
         });
     });
-
 
     $('#printBtn').click(function (e) {
         e.preventDefault();
